9e0a6ff7b3e41b735e0da57b031ba564af5286e2
Nadejda <nadyash295923@gmail.com>
2024-04-10 11:53:23 +0300

    done CF

--- src/main/java/school/mjc/stage0/conditions/task2/CoinFlip.java
+++ src/main/java/school/mjc/stage0/conditions/task2/CoinFlip.java
@@ -2,8 +2,6 @@ package school.mjc.stage0.conditions.task2;
 
 public class CoinFlip {
     public void throwCoin(int from1UpTo1000) {
-/*is less than 500 print "Eagle" otherwise "Tail"
- */
         if (from1UpTo1000 < 500) System.out.println("Eagle");
         else System.out.println("Tail");
     }
